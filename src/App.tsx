636acdd962bf276a5d89bd0f2197f617988845d8
Winicios <winiciosmauri19@hotmail.com>
2022-12-05 21:59:27 -0300

    :tada Create new Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles'
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { useEffect, useState } from 'react';
+import { getCurrentMonth } from './helpers/dateFilter'
 
-function App() {
+
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+
+  }, [list, currentMonth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>
+          Sistema Finaneceiro
+        </C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        {/* Area de informações */}
+
+        {/* Area de inserção */}
+
+        {/* Are de listagem */}
+        
+      </C.Body>
+    </C.Container>
   );
 }
 
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    {
+        date: new Date(2022, 12, 5),
+        category: 'food',
+        title: 'McFonalds',
+        value: 32.12
+    },
+    {
+        date: new Date(2022, 12, 5),
+        category: 'food',
+        title: 'Burguer King',
+        value: 45.12
+    },
+    {
+        date: new Date(2022, 12, 6),
+        category: 'rent',
+        title: 'Aluguel',
+        value: 1500.00
+    },
+    {
+        date: new Date(2022, 12, 7),
+        category: 'salary',
+        title: 'WM&System',
+        value: 7000.00
+    },
+];
